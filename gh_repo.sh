7acab4a164bbcd7a2ac93edce979fbe40788e291
sal <sal@redhat.com>
2014-06-10 15:40:37 +0200

    Made the script more robost in handling github repo name changes

--- gh_repo.sh
+++ gh_repo.sh
@@ -43,72 +43,74 @@ fi
 case "$1" in
 
 clone) echo "Cloning repos"
-       for repo in $REPOS
-           do git clone https://github.com/$MAINTAINER/$repo.git
-       done
-   ;;
+    for repo in ${REPOS}; do 
+        git clone https://github.com/${MAINTAINER}/${repo}.git
+        cd ${repo}
+        git remote set-url origin https://github.com/${USER}/${repo}.git
+        git remote add upstream https://github.com/${USER}/${repo}.git
+        cd -
+    done
+    ;;
 sync)  echo "Syncing repos"
 
 function repo_sync {
-    cd $repo
-    TAG=`git describe --abbrev=0 --tags`
+    cd ${repo}
+    TAG=$(git describe --abbrev=0 --tags)
     git fetch upstream
     git checkout master
     git merge upstream/master
     git push -f origin master --tags
     cd -
     echo
-} 	
+}   
 
 # Update repos
-for repo in $REPOS
+for repo in ${REPOS}
     do
-	if [ -d $repo ]; then
-	    echo -e "Updating $repo"
-	    cd $repo
-	    git config credential.helper store
-	    git pull
-	    cd -
-	    echo
-	else
-	    echo -e "Cloning $repo"
-	    git clone https://github.com/$USER/$repo.git
-	fi 
+    if [ -d ${repo} ]; then
+        echo -e "Updating ${repo}"
+        cd ${repo}
+        git config credential.helper store
+        git pull
+        cd -
+        echo
+    else
+        echo -e "Cloning ${repo}"
+        git clone https://github.com/$USER/$repo.git
+    fi 
 done
 
 # Sync with upstream
-    echo -e "Syncronising $repo"
-	UPSTREAM=`git config -l | grep upstream`
-	if [ -z "$UPSTREAM" ]; then
-	    echo "Initialising Upstream"
-	    git remote add upstream https://github.com/$MAINTAINER/$repo.git
-	    repo_sync
-	else
-	    repo_sync
-	fi
+    echo -e "Syncronising ${repo}"
+    UPSTREAM=`git config -l | grep upstream`
+    if [ -z "${UPSTREAM}" ]; then
+        echo "Initialising Upstream"
+        git remote add upstream https://github.com/${MAINTAINER}/${repo}.git
+        repo_sync
+    else
+        repo_sync
+    fi
     ;;
 scratch) echo  "Deploying from scratch"
         set -x
-	for repo in $REPOS
-	    do rm -rf $WORKSPACE/$repo
-	done
+    for repo in ${REPOS}
+        do rm -rf ${WORKSPACE}/$repo
+    done
 
-	rm -rf ~/.m2
-	$0 sync
+    rm -rf ~/.m2
+    $0 sync
    ;;
 info) echo "Retrieving repo info"
-      for repo in $REPOS
-          do
-              echo -e "Repo info for $repo"
-              cd $repo
-              git remote -v
-              git branch
-              git tag
-              git rev-parse HEAD
-              cd -
-        done
-  ;; 
+    for repo in ${REPOS}; do
+        echo -e "Repo info for ${repo}"
+        cd ${repo}
+        git remote -v
+        git branch
+        git tag
+        git rev-parse HEAD
+        cd -
+    done
+;; 
 *) echo "Usage : $0 {clone|sync|scratch|info}"
    ;;
 esac
-
